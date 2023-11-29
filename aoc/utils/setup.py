30188799522b32bf9d7d55645373c0fd383c13ae
Alex Church <alex@church.id.au>
2023-11-30 08:58:40 +1100

    Updated to write template content to solvers.py.

--- aoc/utils/setup.py
+++ aoc/utils/setup.py
@@ -150,7 +150,7 @@ class Puzzle:
             Path(base_dir, 'input').write_text(input_data)
         
         solvers_file = Path(base_dir, 'solvers.py')
-        solvers_file.touch()
+        solvers_file.write_text(solver_template)
         
         return solvers_file
     
@@ -173,3 +173,19 @@ class Puzzle:
             content = processor(content)
         
         return content
+
+
+solver_template = """from aoc.utils import parsing
+
+input_processor = parsing.split_lines
+
+
+def part1(input_data):
+    
+    raise NotImplementedError()
+
+
+def part2(input_data):
+    
+    raise NotImplementedError()
+"""
